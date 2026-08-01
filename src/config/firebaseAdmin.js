60a8c9c6dba57aafb33d0109320a977635854e76
yeasinarafat51 <aloondokar65@gmail.com>
2026-08-01 17:07:22 +0600

    Fix firebase-admsin version

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,9 +1,11 @@
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 
-const serviceAccount = require("../../firebase/serviceAccountKey.json");
-
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount),
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
