5e466bf8f3656da9181870f6d01b18bfeca3c145
yeasinarafat51 <aloondokar65@gmail.com>
2026-08-01 17:27:41 +0600

    slove

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,11 +1,22 @@
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 
 if (!getApps().length) {
-  initializeApp({
-    credential: cert({
+  let credential;
+
+  if (process.env.FIREBASE_PRIVATE_KEY) {
+    // Vercel
+    credential = cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
+    });
+  } else {
+    // Local
+    const serviceAccount = require("../../firebase/serviceAccountKey.json");
+    credential = cert(serviceAccount);
+  }
+
+  initializeApp({
+    credential,
   });
 }
