7e64bd2c393b09c748a55b102b79295700e9ba26
yeasinarafat51 <aloondokar65@gmail.com>
2026-08-10 14:41:49 +0600

    fire

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -3,15 +3,25 @@ const { initializeApp, cert, getApps } = require("firebase-admin/app");
 if (!getApps().length) {
   let credential;
 
-  if (process.env.FIREBASE_PRIVATE_KEY) {
-    // Vercel
+  // =========================
+  // Vercel / Production
+  // =========================
+  if (
+    process.env.FIREBASE_PROJECT_ID &&
+    process.env.FIREBASE_CLIENT_EMAIL &&
+    process.env.FIREBASE_PRIVATE_KEY
+  ) {
     credential = cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     });
-  } else {
-    // Local Development
+  }
+
+  // =========================
+  // Local Development
+  // =========================
+  else {
     const serviceAccount = require("../../firebase/serviceAccountKey.json");
 
     credential = cert(serviceAccount);
