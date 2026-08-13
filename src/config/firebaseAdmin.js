476645866f36f069b6e97de0fff0b21383edac3c
yeasinarafat51 <aloondokar65@gmail.com>
2026-08-13 21:37:39 +0600

    fire

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -13,7 +13,9 @@ if (!getApps().length) {
   ) {
     credential = cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
+
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     });
   }
@@ -27,6 +29,9 @@ if (!getApps().length) {
     credential = cert(serviceAccount);
   }
 
+  // =========================
+  // Initialize Firebase
+  // =========================
   initializeApp({
     credential,
   });
